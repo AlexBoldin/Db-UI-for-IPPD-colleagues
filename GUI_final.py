415b123d221f1e8221a63a67181b0736713598dd
Alex Boldin <aleksei.boldin@pfizer.com>
2021-09-23 11:44:57 +0100

    Add files via upload

---
+++ GUI_final.py
@@ -0,0 +1,198 @@
+# import sqlite3
+# import mysql.connector
+# import sqlite3
+import pymysql
+# import pandas as pd
+from tkinter import *
+import tkinter.ttk as ttk
+
+#defining function for creating GUI Layout
+def DisplayForm():
+    #creating window
+    display_screen = Tk()
+    #setting width and height for window
+    display_screen.geometry("1200x700")
+    #setting title for window
+    display_screen.title("IPPD Solubility SQL Database alpha")
+    global tree
+    global SEARCH
+    SEARCH = StringVar()
+    #creating frame
+    TopViewForm = Frame(display_screen, width=600, bd=1, relief=SOLID)
+    TopViewForm.pack(side=TOP, fill=X)
+    LeftViewForm = Frame(display_screen, width=600)
+    LeftViewForm.pack(side=LEFT, fill=Y)
+    MidViewForm = Frame(display_screen, width=600)
+    MidViewForm.pack(side=RIGHT)
+    lbl_text = Label(TopViewForm, text="IPPD Solubility SQL Database alpha", \
+                     font=('verdana', 18), width=600,bg="#1C2833",fg="white")
+    lbl_text.pack(fill=X)
+    lbl_txtsearch = Label(LeftViewForm, text="Search", font=('verdana', 15))
+    lbl_txtsearch.pack(side=TOP, anchor=W)
+
+    search = Entry(LeftViewForm, textvariable=SEARCH, font=('verdana', 15), width=10)
+    search.pack(side=TOP, padx=10, fill=X)
+    btn_search = Button(LeftViewForm, text="Search", command=SearchRecord)
+    btn_search.pack(side=TOP, padx=10, pady=10, fill=X)
+    btn_search = Button(LeftViewForm, text="View All", command=DisplayData)
+    btn_search.pack(side=TOP, padx=10, pady=10, fill=X)
+
+    #setting scrollbar
+    scrollbarx = Scrollbar(MidViewForm, orient=HORIZONTAL)
+    scrollbary = Scrollbar(MidViewForm, orient=VERTICAL)
+    tree = ttk.Treeview(MidViewForm,columns=("API", "PF", "PEG_600", \
+    "PEG_400","PEG_300","PEG_200", "WATER", "ETHANOL", "NMP", "DMSO", \
+    "PG", "EXP_VAL", "THEOR_VAL"),
+                        selectmode="extended", height=100, \
+                            yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+    scrollbary.config(command=tree.yview)
+    scrollbary.pack(side=RIGHT, fill=Y)
+    scrollbarx.config(command=tree.xview)
+    scrollbarx.pack(side=BOTTOM, fill=X)
+    #setting headings for the columns
+#    tree.heading('ID', text="ID", anchor=W)
+    tree.heading('API', text="API", anchor=W)
+    tree.heading('PF', text="PF", anchor=W)
+    tree.heading('PEG_600', text="PEG_600", anchor=W)
+    tree.heading('PEG_400', text="PEG_400", anchor=W)
+    tree.heading('PEG_300', text="PEG_300", anchor=W)
+    tree.heading('PEG_200', text="PEG_200", anchor=W)
+    tree.heading('WATER', text="WATER", anchor=W)
+    tree.heading('ETHANOL', text="ETHANOL", anchor=W)    
+    tree.heading('NMP', text="NMP", anchor=W)
+    tree.heading('DMSO', text="DMSO", anchor=W)
+    tree.heading('PG', text="PG", anchor=W)
+    tree.heading('EXP_VAL', text="EXP_VAL", anchor=W)
+    tree.heading('THEOR_VAL', text="THEOR_VAL", anchor=W)
+    
+    #setting width of the columns
+    tree.column('#0', stretch=NO, minwidth=0, width=0)
+    tree.column('#1', stretch=NO, minwidth=0, width=100)
+    tree.column('#2', stretch=NO, minwidth=0, width=100)
+    tree.column('#3', stretch=NO, minwidth=0, width=60)
+    tree.column('#4', stretch=NO, minwidth=0, width=60)
+    tree.column('#5', stretch=NO, minwidth=0, width=60)
+    tree.column('#6', stretch=NO, minwidth=0, width=60)
+    tree.column('#7', stretch=NO, minwidth=0, width=60)
+    tree.column('#8', stretch=NO, minwidth=0, width=80)
+    tree.column('#9', stretch=NO, minwidth=0, width=60)
+    tree.column('#10', stretch=NO, minwidth=0, width=60)
+    tree.column('#11', stretch=NO, minwidth=0, width=40)
+    tree.column('#12', stretch=NO, minwidth=0, width=100)
+    tree.column('#13', stretch=NO, minwidth=0, width=100)
+#    tree.column('#14', stretch=NO, minwidth=0, width=120)
+    
+    tree.pack()
+    DisplayData()
+#function to search data
+def SearchRecord():
+    #checking search text is empty or not
+    if SEARCH.get() != "":
+        #clearing current display data
+        tree.delete(*tree.get_children())
+        #open database
+        #db = sqlite3.connect('Data.db')
+        
+        try:
+            connection = pymysql.connect(
+            host="localhost",
+            user="root",
+            password="1235",
+            db='book2'
+            )
+            cursor = connection.cursor()
+
+
+            # sql3 = "SELECT * FROM `book_details2`"
+            # cursor.execute(sql3)
+            # fetch = cursor.fetchall()
+            
+            
+            cursor.execute("SELECT * FROM book_details2 WHERE API LIKE %s", \
+                           ('%' + str(SEARCH.get()) + '%',))
+            fetch = cursor.fetchall()    
+                
+        #loop for displaying all records into GUI
+            for data in fetch:
+                tree.insert('', 'end', values=(data))
+            cursor.close()
+            
+               
+        finally:    
+            connection.commit()
+            connection.close()
+                
+        
+        #select query with where clause
+        #cursor2 = db.execute("SELECT * FROM book_details2 WHERE API LIKE ?", \
+        #                    ('%' + str(SEARCH.get()) + '%',))
+        #fetch all matching records
+
+        #db.close()
+#defining function to access data from SQLite database
+def DisplayData():
+    #clear current data
+    tree.delete(*tree.get_children())
+    # open databse
+    # db = sqlite3.connect('Data.db')
+    try:
+        connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="1235",
+        db='book2'
+        )
+        cursor = connection.cursor()
+
+        sql3 = "SELECT * FROM `book_details2`"
+        cursor.execute(sql3)
+        fetch = cursor.fetchall()
+    #loop for displaying all records into GUI
+        for data in fetch:
+            tree.insert('', 'end', values=(data))
+        cursor.close()
+        
+        # for data in fetch:
+        #     tree.insert('', 'end', values=(data))        
+        
+    finally:    
+        connection.commit()
+        connection.close()
+
+
+    # #select query
+    # cursor=db.execute("SELECT * FROM INFO")
+    # #fetch all data from database
+    # fetch = cursor.fetchall()
+    #loop for displaying all data in GUI
+
+    # cursor.close()
+    # db.close()
+
+#calling function
+DisplayForm()
+if __name__=='__main__':
+#Running Application
+ mainloop()
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ SQL_DB_update.py
@@ -0,0 +1,66 @@
+import pymysql
+import pandas as pd
+
+data = pd.read_excel("Solubility_Data.xlsx")#, index_col=1)
+data = data.fillna(0)
+
+
+
+try:
+    connection = pymysql.connect(
+      host="localhost",
+      user="root",
+      password="1235"
+    )
+    cursor = connection.cursor()
+    cursor.execute('DROP DATABASE IF EXISTS book2')
+    connection.commit()
+    cursor.execute('CREATE DATABASE book2')
+    connection.commit()
+    cursor.execute('USE book2')
+    connection.commit()
+    
+    sql = """CREATE TABLE book_details2 (
+             API varchar(3000),
+             PF varchar(3000),
+             PEG_600 DECIMAL(19,2), 
+             PEG_400 DECIMAL(19,2),
+             PEG_300 DECIMAL(19,2), 
+             PEG_200 DECIMAL(19,2),
+             WATER DECIMAL(19,2), 
+             ETHANOL DECIMAL(19,2),
+             NMP DECIMAL(19,2), 
+             DMSO DECIMAL(19,2),
+             PG DECIMAL(19,2), 
+             EXP_VAL DECIMAL(19,8),
+             THEOR_VAL DECIMAL(19,8))"""
+    
+    cursor.execute(sql)
+    connection.commit()
+    
+    
+    
+    cols = "`,`".join([str(i) for i in data.columns.tolist()])
+
+# Insert DataFrame recrds one by one.
+    for i,row in data.iterrows():
+        sql2 = "INSERT INTO `book_details2` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+        cursor.execute(sql2, tuple(row))
+    
+        # the connection is not autocommitted by default, so we must commit to save our changes
+    connection.commit()
+    
+    
+    sql3 = "SELECT * FROM `book_details2`"
+    cursor.execute(sql3)
+
+
+finally:
+    
+    connection.commit()
+    connection.close()
+
+    
+    
+    
+    
